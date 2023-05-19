000539ab9dd4130fcaf41f4de5bdb7d59d247cc7
honordevop <ogunladestephen20@gmail.com>
2023-05-19 14:17:05 +0100

    Cypress: Locating elements using .contain(content) and .find(selector)

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -34,7 +34,29 @@ describe("Locator", () =>{
         //Get element with cy data id
         cy.get("[data-cy='btn-id-1']")
         cy.getByTestId('btn-id-1')
+        cy.get("[data-cy='btn-id-1']").contains("Button With Data Test Id")
     })
+
+    //Using contains to locate element
+    it("Get element with contains", ()=>{
+        cy.contains("Unique Text")
+        cy.get("button").contains("Unique Text")
+    
+        //Use contains to search for element with more than one params
+        cy.contains("[type='submit']", "Not Unique Text")
+    })
+
+    //Using .find
+    it("Get element using find", ()=>{
+        cy.get("#form-1").find(".btn-1")
+        
+    })
+
+    
+    
+
+
+
     //Get h3 tag
     /*it("locate h3 tags", ()=>{
         cy.get("h3")

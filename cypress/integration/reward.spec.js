5bf3e0a42dca2c866bbc2ecb8fdcd2111b2bc443
honordevop <ogunladestephen20@gmail.com>
2023-05-20 15:25:33 +0100

    Cypress: Performed a HTTP request testing by intercepting the request to the server and Mocking the expected response

--- cypress/integration/reward.spec.js
+++ cypress/integration/reward.spec.js
@@ -8,4 +8,13 @@ describe("Testing Reward component for HTTP req and res", ()=>{
         cy.get("ul").should("not.be.empty")
         cy.get("ul").should("contain", "500 points for drinking 8 cups of water for 7 straight days").and("contain", "850 points for fasting for 5 days straight")
     })
+
+    //Testing by intercepting the HTTP request and Mocking the HTTP response
+    it("should display a list of rewards from MOCK HTTP response", ()=>{
+        cy.intercept("GET", "http://localhost:4000/rewards", {
+            fixture: "rewards.json"
+        })
+        cy.get("ul").should("not.be.empty")
+        cy.get("ul").should("contain", "500 points for drinking 8 cups of water for 7 straight days").and("contain", "850 points for fasting for 5 days straight")
+    })
 })
